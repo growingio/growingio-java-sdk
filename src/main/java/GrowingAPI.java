d254725347463be8c6b2f176c27a454b2ed6b3dc
wangtong <812609862@qq.com>
2018-11-20 16:43:47 +0800

    init

---
+++ src/main/java/GrowingAPI.java
@@ -0,0 +1,9 @@
+/**
+ * @author : tong.wang
+ * @version : 1.0.0
+ * @since : 11/20/18 12:31 PM
+ */
+public class GrowingAPI {
+
+    public static void trace(){}
+}
---
+++ src/main/java/Message.java
@@ -0,0 +1,9 @@
+/**
+ * @author : tong.wang
+ * @version : 1.0.0
+ * @since : 11/20/18 12:33 PM
+ */
+public class Message {
+
+    
+}
