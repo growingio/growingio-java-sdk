9ee99ba32318001336459a27369cf52a59943999
17hao <sqh1107@gmail.com>
2021-07-29 09:33:18 +0800

    Add http header <Content-Type>

---
+++ src/main/java/io/growing/sdk/java/constants/HttpHeaderConstants.java
@@ -0,0 +1,11 @@
+package io.growing.sdk.java.constants;
+
+public final class HttpHeaderConstants {
+    public static final String CONTENT_TYPE = "Content-Type";
+
+    public static final String CONTENT_LENGTH = "Content-Length";
+
+    public static final String X_COMPRESS_CODEC = "X-Compress-Codec";
+
+    public static final String APPLICATION_JSON = "application/json";
+}
